6a0fbebf157df2837ab639bbad1061f22ae7c1aa
hedgelog <kanarejkikin.com>
2023-01-09 13:47:18 +0300

    first commit

---
+++ Task1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using static System.Net.Mime.MediaTypeNames;
+using System.Net;
+using System.Security.Cryptography;
+using System.Security.Claims;
+
+
+namespace Task1
+{
+    class Filter
+    {
+        string checkCorr;
+        string replaceData;
+
+        Filter(string checkCorr, string replaceData)
+        {
+            this.checkCorr = checkCorr;
+            this.replaceData = replaceData;
+        }
+
+        string CheckCorrectInputData(string inputData)
+        {            
+            checkCorr = @"^(\d*?(\d)\s+\S+\s+)\2\s+";
+            
+            Regex regexp = new Regex(checkCorr);
+
+            if (!regexp.IsMatch(inputData))
+                return inputData;
+            else
+            {
+                return new Regex(checkCorr).Replace(inputData, "$1"); 
+            }                       
+        }        
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputAddress = "140002 ЛЮБЕРЦЫ 2 ОКТЯБРЬСКИЙ ПР 123/4-115";
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "E:/code/QAtest.txt";
+
+            using (StreamReader reader = new StreamReader(path))
+
+                s.Replace(" ", string.Empty);
+
+
+            
+
+            int count = 0;
+            if (Regex.IsMatch(text, "a"))
+            {
+                count++;
+            }
+
+            $ - конец строк.
+        }
+    }
+}
