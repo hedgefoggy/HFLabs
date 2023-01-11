1980a1bfea375d9afc2b748efa02eb4a3a7325a5
hedgelog <hedgeloggy@gmail.com>
2023-01-11 13:35:16 +0300

    last solution

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,14 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Text.RegularExpressions;
-using static System.Net.Mime.MediaTypeNames;
-using System.Net;
-using System.Security.Cryptography;
-using System.Security.Claims;
-
 
 namespace Task1
 {
